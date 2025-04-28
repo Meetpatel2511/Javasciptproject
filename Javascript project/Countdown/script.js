7c67e60d7e4df28c1b69492454265856f40f4773
Meetpatel2511 <mp4297965@gmail.com>
2025-04-28 14:14:45 +0530

    Update script.js

--- Javascript project/Countdown/script.js
+++ Javascript project/Countdown/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("May 5, 2025 00:00:00").getTime();
+var countDownDate = new Date("May 11, 2025 00:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
